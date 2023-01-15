e65ce70d5f7df1f6ca8bd83d2c0bfd79400f0e8e
Mika Terada <wizflounder@gmail.com>
2023-01-16 01:47:18 +0900

    modified fonts

--- header.php
+++ header.php
@@ -6,9 +6,9 @@
         <meta name="description" content="ディスクリプションを入力">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="<?php echo get_template_directory_uri(); ?>/dist/css/style.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Courgette&family=Montserrat:wght@400;500;600&family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP&family=Shippori+Mincho&display=swap" rel="stylesheet">
         <link rel="stylesheet "href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
         <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
         <script src="<?php echo get_template_directory_uri(); ?>/dist/js/scripts.min.js"></script>
